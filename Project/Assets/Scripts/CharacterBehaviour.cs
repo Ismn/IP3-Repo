b743ec39bfc5914324d4bc45ba08014684fc37db
Ross <ross.mcintyre93@gmail.com>
2015-02-10 00:44:41 +0000

    Messing around with the nodes.

--- Project/Assets/Scripts/CharacterBehaviour.cs
+++ Project/Assets/Scripts/CharacterBehaviour.cs
@@ -1,13 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CharacterBehaviour : MonoBehaviour 
 {
+	// Define what the spawnpoint for nodes is.
+	//private GameObject nodeSpawn;
+
+	// Create a list to store these nodes in.
+	//List<NodeBehaviour> nodeList = new List<NodeBehaviour>();
 
 	// Use this for initialization
 	void Start () 
 	{
-
+		// Retrieve the spawn position of every Node in the scene and fill up an array with them.
+		//GameObject[] nodeSpawnPoint = GameObject.FindGameObjectsWithTag("NodeSpawn"); 
+		
+		// Using the length of the array as a parameter, populate the list with the Nodes through instancing.
+		/*for(int i = 0; i < nodeSpawnPoint.Length; i++)
+		{
+			// Instance the the nodes - Instantiate(prefab, position, rotation)
+			//NodeBehaviour nodes = Instantiate(node, this.transform.position, this.transform.rotation) as NodeBehaviour;
+			// Add them to the list
+			//nodeList.Add(nodes);
+			//Debug.Log(nodeList.Count);
+		}*/
 	}
 	
 	// Update is called once per frame
--- Project/Assets/Scripts/NodeBehaviour.cs
+++ Project/Assets/Scripts/NodeBehaviour.cs
@@ -6,7 +6,7 @@ public class NodeBehaviour : MonoBehaviour
 {
 	// Define a node.
 	public GameObject node;
-	// Define the spawnpoint for nodes is.
+	// Define what the spawnpoint for nodes is.
 	private GameObject nodeSpawn;
 
 	// Create a list to store these nodes in.
@@ -16,12 +16,9 @@ public class NodeBehaviour : MonoBehaviour
 	void Start () 
 	{
 		nodeSpawn = this.gameObject;
-
-		// Retrieve the spawn position of every Node in the scene and fill up an array with them.
-		GameObject[] nodeSpawnPoint = GameObject.FindGameObjectsWithTag("NodeSpawn"); 
 		
 		// Using the length of the array as a parameter, populate the list with the Nodes through instancing.
-		for(int i = 0; i < nodeSpawnPoint.Length; i++)
+		for(int i = 0; i < 1; i++)
 		{
 			// Instance the the nodes - Instantiate(prefab, position, rotation)
 			NodeBehaviour nodes = Instantiate(node, this.transform.position, this.transform.rotation) as NodeBehaviour;
