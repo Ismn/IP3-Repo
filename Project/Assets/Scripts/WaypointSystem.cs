b8ca5bb8ec8dc81791eff2ee9568d467d2e7a87f
Ross <ross.mcintyre93@gmail.com>
2015-03-01 17:47:16 +0000

    Fixed "orbiting" issue.

--- Project/Assets/Scripts/WaypointSystem.cs
+++ Project/Assets/Scripts/WaypointSystem.cs
@@ -17,48 +17,16 @@ using System.Collections.Generic;
 
 public class WaypointSystem : MonoBehaviour
 {	
-	// This is the rate of accelleration after the function "Accelerate()" is called.
-	// Higher values will cause the object to reach the "speedLimit" in less time.
-	public float acceleration = 0.8f;
-	
-	// This is the the amount of velocity retained after the function "Slow()" is called.
-	// Lower values cause quicker stops. A value of "1.0" will never stop. Values above "1.0" will speed up.
-	public float inertia = 0.9f;
-	
 	// This is as fast the object is allowed to go.
 	public float speedLimit = 10.0f;
-	
-	// This is the speed that tells the functon "Slow()" when to stop moving the object.
-	public float minSpeed = 1.0f;
-	
-	// This is how long to pause inside "Slow()" before activating the Accelerate function.
-	public float stopTime = 1.0f;
 
-	private float currentSpeed = 0.0f;
-	
-	// The variable "functionState" controlls which function, "Accelerate()" or "Slow()",
-	// is active. "0" is function "Accelerate()" and "1" is function "Slow()".
-	private float functionState = 0;
-	
-	// The next two variables are used to make sure that while the function "Accelerate()" is running,
-	// the function "Slow()" can not run (as well as the reverse).
-	private bool accelerationState;
-	private bool slowState;
+	private float speed = 5.0f;
 	
 	// This variable will store the "active" target object (the waypoint to move to).
-	private GameObject waypoint;
-	
-	// This is the speed the object will rotate to face the active Waypoint.
-	public float rotationDamping = 6.0f;
-	
-	// If this is false, the object will rotate instantly toward the Waypoint.
-	// If true, you get smoooooth rotation baby!
-	public bool smoothRotation = true;
+	private Transform currentWaypoint;
 
 	// Holds all the Waypoint Objects that you assign in the inspector.
-	public List<GameObject> waypoints;
-
-	public Quaternion rotation;
+	public List<Transform> waypoints;
 	
 	// This variable keeps track of which Waypoint Object,
 	// in the previously mentioned array variable "waypoints", is currently active.
@@ -67,113 +35,29 @@ public class WaypointSystem : MonoBehaviour
 	//The function "Start()" is called just before anything else but only one time.
 	void Start( )
 	{
-		// When the script starts set "0" or function Accelerate() to be active.
-		functionState = 0;
 	}
 	
 	//The function "Update()" is called every frame. It can get slow if overused.
 	void Update ()
-	{
-		// If functionState variable is currently "0" then run "Accelerate()".
-		// Withouth the "if", "Accelerate()" would run every frame.
-		if (functionState == 0)
-		{
-			Accelerate();
-		}
-		
-		// If functionState variable is currently "1" then run "Slow()".
-		// Withouth the "if", "Slow()" would run every frame.
-		if (functionState == 1)
-		{
-			StartCoroutine(Slow());
-		}
-		
-		waypoint = waypoints[WPindexPointer]; //Keep the object pointed toward the current Waypoint object.
-	}
-	
-	// I declared "Accelerate()".
-	void Accelerate ()
-	{
-		if (accelerationState == false)
-		{
-			// Make sure that if Accelerate() is running, Slow() can not run.
-			accelerationState = true;
-			slowState = false;
-		}
-
-		
-		// I grabbed this next part from the unity "SmoothLookAt" script but try to explain more.
-		if (waypoint) //If there is a waypoint do the next "if".
-		{
-			if (smoothRotation)
-			{
-				// Look at the active waypoint.
-				rotation = Quaternion.LookRotation(waypoint.transform.position - transform.position);
-				
-				// Make the rotation nice and smooth.
-				// If smoothRotation is set to "On", do the rotation over time
-				// with nice ease in and ease out motion.
-				transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationDamping);
-			}
-		}
-		
-		// Now do the accelleration toward the active waypoint untill the "speedLimit" is reached
-		currentSpeed = currentSpeed + acceleration * acceleration;
-		transform.Translate (0,0,Time.deltaTime * currentSpeed);
-		
-		// When the "speedlimit" is reached or exceeded ...
-		if (currentSpeed >= speedLimit)
-		{
-			// ... turn off accelleration and set "currentSpeed" to be
-			// exactly the "speedLimit". Without this, the "currentSpeed
-			// will be slightly above "speedLimit"
-			currentSpeed = speedLimit;
-		}
+	{		
+		currentWaypoint = waypoints[WPindexPointer]; //Keep the object pointed toward the current Waypoint object.
+		Debug.Log(WPindexPointer);
+		transform.position = Vector3.MoveTowards(transform.position, currentWaypoint.position, speed * Time.deltaTime);
 	}
 	
 	//The function "OnTriggerEnter" is called when a collision happens.
 	void OnTriggerEnter ()
-	{
-		// When the GameObject collides with the waypoint's collider,
-		// activate "Slow()" by setting "functionState" to "1".
-		functionState = 1;
-		
+	{		
 		// When the GameObject collides with the waypoint's collider,
 		// change the active waypoint to the next one in the array variable "waypoints".
 		WPindexPointer++;
-		
+		Debug.Log(WPindexPointer);
 		// When the array variable reaches the end of the list ...
 		if (WPindexPointer >= waypoints.Count)
 		{
 			// ... reset the active waypoint to the first object in the array variable
 			// "waypoints" and start from the beginning.
 			WPindexPointer = 0;
-		}
-	}
-	
-	// I declared "Slow()".
-	IEnumerator Slow()
-	{
-		if (slowState == false) //
-		{
-			// Make sure that if Slow() is running, Accelerate() can not run.
-			accelerationState = false;
-			slowState = true;
-		}
-		
-		// Begin to do the slow down (or speed up if inertia is set above "1.0" in the inspector).
-		currentSpeed = currentSpeed * inertia;
-		transform.Translate (0,0,Time.deltaTime * currentSpeed);
-		
-		// When the "minSpeed" is reached or exceeded ...
-		if (currentSpeed <= minSpeed)
-		{
-			// ... Stop the movement by setting "currentSpeed to Zero.
-			currentSpeed = 0.0f;
-			// Wait for the amount of time set in "stopTime" before moving to next waypoint.
-			yield return new WaitForSeconds(stopTime);
-			// Activate the function "Accelerate()" to move to next waypoint.
-			functionState = 0;
 		}
 	} 
 }
