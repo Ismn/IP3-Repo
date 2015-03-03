797a0c2d4be73adf7e90205e6ee6b20cb6db153c
Ismn <ssingl200@lab-m627-osx-17.local>
2015-03-03 11:23:51 +0000

    Added project

---
+++ Mary's Meals/Assets/Scripts/mainMenuScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainMenuScript : MonoBehaviour {
+
+	public GUISkin allUI;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (Input.GetKeyDown ("escape")) {
+			Application.Quit();
+				}
+	}
+
+	void OnGUI (){
+		GUI.skin = allUI;
+		if (GUI.Button (new Rect (Screen.width / 2 - 100, Screen.height / 2 - 100, 200, 100), "", GUI.skin.GetStyle ("PlayGameButton"))) {
+			Application.LoadLevel("MainGame");
+				}
+		if (GUI.Button (new Rect (Screen.width / 2 - 100, Screen.height / 2 + 50, 200, 100), "", GUI.skin.GetStyle ("QuitGameButton"))) {
+			Application.Quit ();
+		}
+		if (GUI.Button (new Rect (Screen.width - 100, 0, 100, 100), "", GUI.skin.GetStyle ("GameAudioButton"))) {
+			
+		}
+		if (GUI.Button (new Rect (Screen.width - 200, 0, 100, 100), "", GUI.skin.GetStyle ("GameMusicButton"))) {
+			
+		}
+		}
+}
