484f55e1ce62d310b5c28bc70f58dfc387edaa21
Crystalix <e.gillespie42@hotmail.com>
2015-03-12 18:55:20 +0000

    start and quit button done

--- Project/Assets/Scripts/mainMenuScript.cs
+++ Project/Assets/Scripts/mainMenuScript.cs
@@ -1,35 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class mainMenuScript : MonoBehaviour {
+public class mainMenuScript : MonoBehaviour
+{
 
 	public GUISkin allUI;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	if (Input.GetKeyDown ("escape")) {
-			Application.Quit();
-				}
-	}
-
-	void OnGUI (){
-		GUI.skin = allUI;
-		if (GUI.Button (new Rect (Screen.width / 2 - 100, Screen.height / 2 - 100, 200, 100), "", GUI.skin.GetStyle ("PlayGameButton"))) {
-			Application.LoadLevel("Wireframe - Test");
-				}
-		if (GUI.Button (new Rect (Screen.width / 2 - 100, Screen.height / 2 + 50, 200, 100), "", GUI.skin.GetStyle ("QuitGameButton"))) {
+	void Update ()
+	{
+		if (Input.GetKeyDown ("escape")) {
 			Application.Quit ();
 		}
-		if (GUI.Button (new Rect (Screen.width - 100, 0, 100, 100), "", GUI.skin.GetStyle ("GameAudioButton"))) {
-			
-		}
-		if (GUI.Button (new Rect (Screen.width - 200, 0, 100, 100), "", GUI.skin.GetStyle ("GameMusicButton"))) {
-			
-		}
-		}
+	}
+
+	void OnGUI ()
+	{
+//		GUI.skin = allUI;
+//		if (GUI.Button (new Rect (Screen.width / 2 - 100, Screen.height / 2 - 100, 200, 100), "", GUI.skin.GetStyle ("PlayGameButton"))) {
+//			Application.LoadLevel ("Wireframe - Test");
+//		}
+//		if (GUI.Button (new Rect (Screen.width / 2 - 100, Screen.height / 2 + 50, 200, 100), "", GUI.skin.GetStyle ("QuitGameButton"))) {
+//			Application.Quit ();
+//		}
+//		if (GUI.Button (new Rect (Screen.width - 100, 0, 100, 100), "", GUI.skin.GetStyle ("GameAudioButton"))) {
+//			
+//		}
+//		if (GUI.Button (new Rect (Screen.width - 200, 0, 100, 100), "", GUI.skin.GetStyle ("GameMusicButton"))) {
+//			
+//		}
+	}
+	public void PlayButton ()
+	{
+		Application.LoadLevel ("Wireframe - Test");
+	}
+
+	public void QuitButton ()
+	{
+		Application.Quit ();
+	}
 }
