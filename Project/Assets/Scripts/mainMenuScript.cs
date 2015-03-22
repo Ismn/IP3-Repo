46e65be8586cd482dced66b57675a2b4dc8d9ed9
McQueen <youngconnorjohnston@googlemail.com>
2015-03-22 01:46:08 +0000

    Just Changes to Main Menu

--- Project/Assets/Scripts/mainMenuScript.cs
+++ Project/Assets/Scripts/mainMenuScript.cs
@@ -5,6 +5,22 @@ public class mainMenuScript : MonoBehaviour
 {
 
 	public GUISkin allUI;
+	public GameObject playButton;
+	public GameObject quitButton;
+	public GameObject fullBackground;
+	public GameObject background;
+	public GameObject africaUnlocked;
+	public GameObject africaHighlighted;
+	public GameObject africaLocked;
+	public GameObject asiaUnlocked;
+	public GameObject asiaHighlighted;
+	public GameObject asiaLocked;
+	public GameObject southAmericaUnlocked;
+	public GameObject southAmericaHighlighted;
+	public GameObject southAmericaLocked;
+	public GameObject carribeanUnlocked;
+	public GameObject carribeanHighlighted;
+	public GameObject carribeanLocked;
 
 	// Use this for initialization
 	void Start ()
@@ -38,11 +54,23 @@ public class mainMenuScript : MonoBehaviour
 	}
 	public void PlayButton ()
 	{
-		Application.LoadLevel ("Wireframe - Test");
+		fullBackground.SetActive (false);
+		playButton.SetActive (false);
+		quitButton.SetActive (false);
+		asiaUnlocked.SetActive (true);
+		africaLocked.SetActive (true);
+		carribeanLocked.SetActive (true);
+		southAmericaLocked.SetActive (true);
+		background.SetActive (true);
+
 	}
 
 	public void QuitButton ()
 	{
 		Application.Quit ();
+	}
+
+	public void AfricaPlayButton(){
+		Application.LoadLevel ("Wireframe - Test");
 	}
 }
