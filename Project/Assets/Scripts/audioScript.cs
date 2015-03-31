7b446594c4be6f95878af78abfcffe7b11928be7
McQueen <youngconnorjohnston@googlemail.com>
2015-03-31 21:06:21 +0100

    Just a quick commit

--- Project/Assets/Scripts/audioScript.cs
+++ Project/Assets/Scripts/audioScript.cs
@@ -3,13 +3,34 @@ using System.Collections;
 
 public class audioScript : MonoBehaviour {
 
+	public AudioClip track1;
+	public AudioClip track2;
+
 	// Use this for initialization
 	void Start () {
-	
+		audio.Play ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		/*if (Application.loadedLevelName != ("MainMenu")) {
+			audio.loop = false;
+			if(audio.isPlaying!=true){
+				audio.clip = track2;
+				audio.Play();
+			}
+
+		}*/
 		GameObject.DontDestroyOnLoad (gameObject);
+	}
+
+	IEnumerator CycleAudio(){
+		yield return new WaitForSeconds (track1.length);
+		audio.clip = track2;
+		audio.Play();
+		yield return new WaitForSeconds (track2.length);
+		audio.clip = track1;
+		audio.Play ();
+		CycleAudio ();
 	}
 }
